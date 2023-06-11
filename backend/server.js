7d1503caf2603c306ae869e0649a271fc2ba2b12
warriorBunny013 <mona23sonai@gmail.com>
2023-06-11 17:35:51 +0530

    inital

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,7 @@ const app=express();
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
-app.use(cors({
-  origin:["https://taskify-mern-frontend.vercel.app"],
-  methods:["POST","GET","PATCH","PUT","DELETE"],
-  credentials:true
-}));
+app.use(cors());
 
 //mongodb connection
 const connection=async()=>{
