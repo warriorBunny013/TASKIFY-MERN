1dc703a1bc3a9c0242a02d06b2673a713109fa0b
warriorBunny013 <mona23sonai@gmail.com>
2023-07-15 03:50:53 +0530

    added hash router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,16 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 window.visitscount_overview=0;
 root.render(
   // <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
     </Provider>
   // </React.StrictMode>
 );
