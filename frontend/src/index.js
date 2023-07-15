a1e0502be7b30df2d64e8cbca0e94abd9f124d0f
warriorBunny013 <mona23sonai@gmail.com>
2023-07-15 13:29:55 +0530

    changed hashrouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,16 +5,16 @@ import App from './App';
 // import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-import { HashRouter} from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // window.visitscount_overview=0;
 root.render(
   // <React.StrictMode>
     <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
     <App />
-    </HashRouter>
+    </BrowserRouter>
     </Provider>
   // </React.StrictMode>
 );
